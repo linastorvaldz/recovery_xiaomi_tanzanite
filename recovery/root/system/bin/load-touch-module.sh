cd44eb3d7323fadf47f9bbbc01687d55559a21aa
~J3mbo4D~ <creamylofly@gmail.com>
2025-11-02 13:11:16 +0800

    Comment out dependent kernel modules in load-touch-module.sh
    Comment out modules that have dependencies to avoid loading issues.

--- recovery/root/system/bin/load-touch-module.sh
+++ recovery/root/system/bin/load-touch-module.sh
@@ -8,13 +8,13 @@ set +e
 
 mod_dir="/lib/modules"
 mods=(
-  mtk-mbox.ko # has no depends
+  #mtk-mbox.ko # has no depends
   switch_class.ko # has no depends
   lct_tp.ko # has no depends
   mtk-afe-external.ko # has no depends
   xiaomi.ko # has no depends
-  mtk_rpmsg_mbox.ko # depends on mtk-mbox
-  mtk_tinysys_ipi.ko # depends on mtk-mbox and mtk_rpmsg_mbox
+  #mtk_rpmsg_mbox.ko # depends on mtk-mbox
+  #mtk_tinysys_ipi.ko # depends on mtk-mbox and mtk_rpmsg_mbox
   scp.ko # depends on mtk_tinysys_ipi , mtk-mbox, mtk-afe-external
   ft3519t.ko # finally, load the touch driver after loading its dependencies
 )
